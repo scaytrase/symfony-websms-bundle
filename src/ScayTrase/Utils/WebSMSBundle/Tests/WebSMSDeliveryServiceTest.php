d5449e72f318092e1935554862bb97dbda4af36f
Pavel Batanov <pavel@batanov.me>
2014-08-02 13:57:07 +0400

    Profiler test

--- src/ScayTrase/Utils/WebSMSBundle/Tests/WebSMSDeliveryServiceTest.php
+++ src/ScayTrase/Utils/WebSMSBundle/Tests/WebSMSDeliveryServiceTest.php
@@ -8,10 +8,14 @@
 
 namespace ScayTrase\Utils\WebSMSBundle\Tests;
 
+use ScayTrase\Utils\SMSDeliveryBundle\DataCollector\MessageDeliveryDataCollector;
 use ScayTrase\Utils\SMSDeliveryBundle\DependencyInjection\SMSDeliveryExtension;
 use ScayTrase\Utils\SMSDeliveryBundle\Service\ShortMessageInterface;
 use ScayTrase\Utils\WebSMSBundle\DependencyInjection\WebSMSExtension;
+use ScayTrase\Utils\WebSMSBundle\Service\WebSMSDeliveryService;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class WebSMSDeliveryServiceTest extends \PHPUnit_Framework_TestCase
 {
@@ -48,6 +52,35 @@ class WebSMSDeliveryServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($sender->send($message));
     }
 
+    public function testCollector()
+    {
+        $delivery_extension = new SMSDeliveryExtension();
+        $websms_extension = new WebSMSExtension();
+
+        $container = $this->getContainer();
+
+        $delivery_extension->load(array(array('disable_delivery' => true)), $container);
+        $websms_extension->load(array(), $container);
+
+        $container->setParameter('sms_delivery.class', 'ScayTrase\Utils\WebSMSBundle\Service\WebSMSDeliveryService');
+        /** @var WebSMSDeliveryService $sender */
+        $sender = $container->get('sms_delivery.sender');
+
+        $collector = new MessageDeliveryDataCollector($sender);
+
+
+        /** @var ShortMessageInterface|\PHPUnit_Framework_MockObject_MockObject $message */
+        $message = $this->getMock('ScayTrase\Utils\SMSDeliveryBundle\Service\ShortMessageInterface');
+
+        $this->assertTrue($sender->send($message));
+        $this->assertTrue($sender->send($message));
+
+        $collector->collect(new Request(), new Response());
+
+        $this->assertCount(2,$collector->getRecords());
+        $this->assertEquals('ScayTrase\Utils\WebSMSBundle\Service\WebSMSDeliveryService',$collector->getService());
+    }
+
     public function testConfigurationMerging()
     {
         $delivery_extension = new SMSDeliveryExtension();
@@ -65,6 +98,10 @@ class WebSMSDeliveryServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('ScayTrase\Utils\WebSMSBundle\Service\WebSMSDeliveryService', $sender);
     }
 
+    public function testSending()
+    {
+        $this->markTestIncomplete('Have to stub file_get_content');
+    }
 
     private function getContainer()
     {
