9a18154edd88776052d136d080e1fe886c77b4e4
Pavel Batanov <batanov@banki.ru>
2015-07-21 18:54:31 +0300

    Fully refactored as bridge using scaytrase/websms-php

---
+++ src/ScayTrase/WebSmsBridge/WebSmsBridgeBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: batanov.pavel
+ * Date: 21.07.2015
+ * Time: 17:54
+ */
+
+namespace ScayTrase\WebSmsBridge;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebSmsBridgeBundle extends Bundle
+{
+
+}
